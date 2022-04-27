6eb7c6d7b7d20963e8d0d683ef56e9f4812c6c1d
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-04-27 15:07:50 -0300

    Feito requisito 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+import * as api from './services/api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  //= > { console.log(categories); })
+  componentDidMount() {
+    api.getCategories().then((categories) => { console.log(categories); });
+  }
+
+  /*  recebeAPI = async () => {
+    const api = await getCategories();
+    this.setState({
+      fetchAPI: api,
+    });
+  } */
+
+  render() {
+    // console.log(fetchAPI);
+    return (
+      <div>
+        Trabalho
+      </div>
+    );
+  }
 }
 
 export default App;
