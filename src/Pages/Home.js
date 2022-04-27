9fd430ff3e6134f4ffeae2167bcda63c300e4c1a
Aline Soliman <alineapreis@hotmail.com>
2022-04-27 18:11:14 -0300

    alterações requisito 3

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
+        <img href="/shoppingcart" src="https://svgsilh.com/svg/155226.svg" alt="carrinho-de-compras" />
       </div>
     );
   }
