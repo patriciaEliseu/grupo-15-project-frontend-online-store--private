8e9b0497446a042d2658ae6e8078cb21e6603364
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-04-28 15:35:51 -0300

    Fazendo requisito 5 - Liste os produtos buscados por termos, com os dados resumidos, associados a esses termos

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as api from '../services/api';
+import CardProdutos from './CardProdutos';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      inputName: '',
+      produtosInput: [],
     };
   }
 
@@ -19,13 +22,55 @@ class Home extends Component {
     this.setState({ categories: apiGetCategories });
   }
 
+  getValorInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.setState({
+        inputName: value,
+      });
+      /* const { inputName } = this.state;
+    console.log(inputName.length > 2); */
+    });
+    /* const { inputName } = this.state;
+    console.log(inputName.length > 2); */
+  }
+
+  getPesquisar = async (event) => {
+    event.preventDefault();
+    // title,thumbnail, price
+    const { inputName } = this.state; // já chega modificado aqui
+    const produtos = await api.getProductsFromCategoryAndQuery('', inputName);
+    const { results } = produtos;
+    this.setState({
+      produtosInput: results,
+    });
+    /*  const { produtosInput } = this.state;
+    console.log(produtosInput); */
+
+    /* console.log(inputName);
+    console.log(' teste func ok!'); */
+  }
+
   render() {
-    const { categories } = this.state;
+    const { categories, inputName, produtosInput } = this.state;
+    console.log(produtosInput);
     return (
       <div>
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
+        <form data-testid="query-button" onSubmit={ this.getPesquisar }>
+          <label htmlFor="input">
+            Pesquisar
+            <input
+              onChange={ this.getValorInput }
+              data-testid="query-input"
+              name="inputName"
+              type="text"
+              value={ inputName }
+            />
+          </label>
+        </form>
         <Link
           to="/shoppingcart"
           data-testid="shopping-cart-button"
@@ -50,9 +95,32 @@ class Home extends Component {
             </label>
           </div>
         ))}
+        {// title,thumbnail, price
+          produtosInput.length === 0 ? <div> Nenhum produto foi encontrado </div>
+            : produtosInput.map(({ title, thumbnail, price, id }, index) => (
+
+              <CardProdutos
+                key={ index }
+                keyNumber={ id }
+                title={ title }
+                thumbnail={ thumbnail }
+                price={ price }
+              />
+            ))
+        }
       </div>
     );
   }
 }
 
 export default Home;
+/* {
+          produtosInput.map((elemento) => (
+            <div key={ elemento }>
+              {
+                elemento
+              }
+
+            </div>
+          ))
+        } */
